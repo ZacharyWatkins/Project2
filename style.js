8ebd4be6b2070fc23e6b7dab51541faaa252adb9
Zachary Watkins <Zachary Watkins>
2016-11-04 18:08:33 -0700

    Making repo, testing cookie

---
+++ style.js
@@ -0,0 +1,32 @@
+var tester
+function submit(){
+  tester = document.getElementById("choice").value;
+  if (isNaN(tester)) {
+    window.alert("Must be a Number")
+  }
+  else {
+    setCookie("idea",tester);
+  }
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
